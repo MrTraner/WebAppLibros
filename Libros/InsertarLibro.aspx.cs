9cf677a063e4f0a188e0cf7debb33cfce78be214
Mauricio Ochoa <juanmauricio.ochoa@hotmail.com>
2018-08-02 16:27:24 -0500

    Update InsertarLibro.aspx.cs

--- Libros/InsertarLibro.aspx.cs
+++ Libros/InsertarLibro.aspx.cs
@@ -27,27 +27,32 @@ namespace WebAppLibros.Libros
 
         void btnInsertar_Click(object sender, EventArgs e)
         {
-            Response.Write("<script>alert('Libro insertado');</script>");
+            //Response.Write("<script>alert('Libro insertado');</script>");
             if (Request.Params.Get("Titulo") != "" && Request.Params.Get("Autor") != "" && Request.Params.Get("Descripcion") != "" && Request.Params.Get("TotalPaginas") != "" && Request.Params.Get("Precio") != "")
             {
                 try
                 {
-                    Libro_Acciones libroAccionesObjeto = Libro_Acciones.GetInstancia();
-
                     Libro libro = new Libro();
-
-                    libro.IdLibro = libroAccionesObjeto.Consulta_General().Count + 1;
                     libro.Titulo = Request.Params.Get("Titulo");
                     libro.Autor = Request.Params.Get("Autor");
                     libro.Descripcion = Request.Params.Get("Descripcion");
                     libro.TotalPaginas = int.Parse(Request.Params.Get("TotalPaginas"));
                     libro.Precio = double.Parse(Request.Params.Get("Precio"));
 
-                    libroAccionesObjeto.Insertar(libro);
+                    int resultado = Libro_Acciones.Insertar(libro);
 
-                    Response.Write("<script>alert('Libro insertado');</script>");
+                    if (resultado > 0)
+                    {
+                        Response.Write("<script>alert('Libro insertado');</script>");
 
-                    Response.Redirect("ConsultarLibros.aspx");
+                        Response.Redirect("ConsultarLibros.aspx");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('No se ha insertado el libro');</script>");
+
+                        Response.Redirect("ConsultarLibros.aspx");
+                    }
                 }
                 catch (Exception error)
                 {
